8c7f167f0bf72718cd6fa8df767bad27fe01bbcc
Bratapfel2000 <43928387+Bratapfel2000@users.noreply.github.com>
2019-08-16 13:31:27 +0200

    Update TicTacToe.py

--- TicTacToe.py
+++ TicTacToe.py
@@ -14,18 +14,17 @@ def possible_numbers():
     else:
         print("Wrong input. Try again")
         return possible_numbers()
-
+    
 #create 9 fields
 field = []
 for i in range(9):
     field.append(str(i+1))
-
+    
 #create board with the 9 fields
 def board():
     print(field[0],field[1],field[2])
     print(field[3],field[4],field[5])
     print(field[6],field[7],field[8])
-##board()
 
 #define win situation
 def win():
@@ -36,64 +35,82 @@ def win():
     if field[0]==field[4]==field[8] or field[2]==field[4]==field[6]:
         return True
 
-# player x. enter number with possible_numbers().
-# If field occupied go back to start.
-# If not, assign place with "X".
-# if win() is True, player wins.
-# If not win() is True, call player "O" tictac_o()
+#define when board is full without winner
+def full():
+    if ((field[0] == "X" or  field[0] == "O") and
+        (field[1] == "X" or  field[1] == "O") and
+        (field[2] == "X" or  field[2] == "O") and
+        (field[3] == "X" or  field[3] == "O") and
+        (field[4] == "X" or  field[4] == "O") and
+        (field[5] == "X" or  field[5] == "O") and
+        (field[6] == "X" or  field[6] == "O") and
+        (field[7] == "X" or  field[7] == "O") and
+        (field[8] == "X" or  field[8] == "O")):
+        print(" ")
+        board()
+        print("-----------No Winner! Game Over!--------------")
+        return True
+    else:
+        return False
 
-def tictac_x():
-    print("Player 'X's turn")
-    eingabe = possible_numbers()
-    eingabe_int = int(eingabe)
+#swap player
+def otherplayer(player):
+    if player is "X":
+        return tictac_z("O")
+    if player == "O":
+        return tictac_z("X")
+
+#chose field on board
+def move(player, eingabe_int):
     if field[eingabe_int-1] == "X" or field[eingabe_int-1] == "O":
         print("Already occupied. Try again")
-        tictac_x()
+        otherplayer(player)    
     else:
-        field[eingabe_int-1] = "X"
+        field[eingabe_int-1] = player
+
+
         if win() == True:
             print(" ")
             board()
-            print("Player 'X' wins!")
+            print("Player '" + player + "' wins!")
+        elif full() == True:
+            return tictac_retry()
         else:
             print(" ")
             board()
-            tictac_o()
-            
-# Same procedure as with player "X"        
-def tictac_o():
-    print("Player 'O's turn")
+            otherplayer(player)
+
+#player x / o          
+def tictac_z(player):
+    print("Player '"+ player+"'s turn")
     eingabe = possible_numbers()
     eingabe_int = int(eingabe)
-    if field[eingabe_int-1] == "X" or field[eingabe_int-1] == "O":
-        print("Already occupied. Try again")
-        tictac_o()
-    else:
-        field[eingabe_int-1] = "O"
-        if win() == True:
-            print(" ")
-            board()
-            print("Player 'O' wins!")
-        else:
-            print(" ")
-            board()
-            tictac_x()
+    move(player, eingabe_int)
 
-            
-#Chose player for start
+#start game
 def tictactoe():
     eingabe = input("Choose Player [X/O]: ")
     if eingabe == "X" or eingabe == "x":
         print(" ")
         board()
-        return tictac_x()
+        return tictac_z("X")
     if eingabe == "O" or eingabe == "o" or eingabe == "0":
         print(" ")
         board()
-        return tictac_o()
+        return tictac_z("O")
     else:
-        print("Input not valid. Try again")
+        print("Input not valid. Try again.")
         return tictactoe()
 
-        
+#game over in case of board full
+def tictac_retry():
+    eingabe = input("Play again? [Y/N]: ")
+    if eingabe == "Y" or eingabe == "y":
+        return tictactoe()
+    if eingabe == "N" or eingabe == "n":
+        print("----- Bye ;) -----")
+    else:
+        print("Input not valid. Try again.")
+        return tictac_retry()
+    
 tictactoe()
